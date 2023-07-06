3b411278f3700ba384cb8a41f2ff1f04033e4c43
Michael Bethel <michaelscott199410@gmail.com>
2023-07-06 08:28:57 +0100

    I updated and modified landing page topbar

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ navbutton.addEventListener('click', ()=>{
 
 const width = innerWidth;
 console.log(width);
+
+
+function changetopnav(){
+    var scrollvalue = window.scrollY;
+    var topbar =  document.getElementById('topnav-fixed');
+    var searchItem = document.getElementById('searchItem');
+    console.log(scrollvalue);
+    if(scrollvalue < 100){
+        topbar.classList.remove('topbg')
+        // alert("We have not scrolled yet")
+
+    }else{
+        topbar.classList.add('topbg');
+        topbar.style.transition = 'all ease 3s';
+    }
+    // if (scrollvalue < 100) {
+    //     topbar.classList.remove('topbg');
+    //     searchItem.classList.remove('searchbarBg');
+
+    // }
+    // else{
+    //     topbar.classList.add('topbg');
+    //     searchItem.classList.add('searchbarBg');
+    // }
+}
+
+window.addEventListener('scroll', changetopnav);
